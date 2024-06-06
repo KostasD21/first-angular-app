cd2d19c566abbb1236eb3c9d9b6df81a59a77c31
KostasD21 <kostasdouzis@gmail.com>
2024-06-06 23:27:43 +0300

    first commit

---
+++ src/app/customers/customers.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { CustomersComponent } from "./customers.component";
+import { CustomersListComponent } from "./customers-list/customers-list.component";
+
+@NgModule({
+    declarations: [ CustomersComponent, CustomersListComponent ],
+    exports: [ CustomersComponent, CustomersListComponent ]
+})
+export class CustomersModule { }
