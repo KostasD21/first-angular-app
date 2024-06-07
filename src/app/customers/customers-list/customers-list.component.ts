29fc1b614e7a55a6b37a54974a1d859429e773ec
KostasD21 <kostasdouzis@gmail.com>
2024-06-07 22:18:51 +0300

    Changes regarding event /property bindings and input properties

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { ICustomer } from "../../shared/interfaces";
 
 @Component({
     selector: 'app-customers-list',
@@ -6,13 +7,37 @@ import { Component, OnInit } from "@angular/core";
 })
 export class CustomersListComponent implements OnInit {
 
-    filteredCustomers: any[] = [];
+    private _customers: ICustomer[] = [];
+    @Input() get customers(): ICustomer[] {
+        return this._customers;
+    }
+    
+    set customers(value: ICustomer[]) {
+        if (value) {
+            this.filteredCustomers = this._customers = value;
+            this.calculateOrders();
+        }
+    }
+
+    filteredCustomers: ICustomer[] = [];
     customersOrderTotal: number | undefined;
     currencyCode: string = 'USD';
 
     constructor() { }
 
     ngOnInit() {
+    }
 
+    calculateOrders() {
+        this.customersOrderTotal = 0;
+        this.filteredCustomers.forEach((cust: ICustomer) => {
+            if (cust.orderTotal && this.customersOrderTotal) {
+                this.customersOrderTotal += cust.orderTotal;
+            }
+        });
+    }
+
+    sort(prop: string) {
+        // A sorter service will handle the sorting
     }
 }
